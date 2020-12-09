a32260baaa8b32f77d40f40cd02515863724ebf7
giselamo <gisemo@gmail..com>
2020-12-08 23:08:48 -0300

    Add victim create script

--- scripts/victim.sql
+++ scripts/victim.sql
@@ -1,6 +1,21 @@
-CREATE TABLE victims (
-  id SERIAL,
-  age INT,
-  first_name VARCHAR(255),
-  last_name VARCHAR(255)
-);
+-- Table: public.victims
+
+-- DROP TABLE public.victims;
+
+CREATE TABLE public.victims
+(
+    id bigint NOT NULL,
+    age bigint,
+    first_name character varying(255) COLLATE pg_catalog."default",
+    last_name character varying(255) COLLATE pg_catalog."default",
+    gender character varying(20) COLLATE pg_catalog."default",
+    nationality character varying(3) COLLATE pg_catalog."default",
+    CONSTRAINT victims_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.victims
+    OWNER to vivas;
+
+    
