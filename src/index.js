7574d6f77c6a8648a7bd9d2d7f7353d0f96f560c
Andres Arana <and2arana@gmail.com>
2020-12-20 18:20:23 -0300

    Sets up basic koa application

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Gigi RE genia");
+const Koa = require("koa");
+
+const Router = require('@koa/router');
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/gigi', async (ctx) => {
+  ctx.body = "Genia";
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(8080);
