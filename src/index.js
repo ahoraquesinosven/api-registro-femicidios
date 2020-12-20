b73a85e08d3e4b3be3d53ccd1f6005bf3b40728d
giselamo <gisemo@gmail..com>
2020-12-20 19:27:28 -0300

    Adds get and post Case basic endpoints

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 const Koa = require("koa");
-
 const Router = require('@koa/router');
+const bodyParser = require('koa-bodyparser');
+
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/gigi', async (ctx) => {
-  ctx.body = "Genia";
+app.use(bodyParser());
+const cases = [];
+
+router.get('/v1/cases', async (ctx) => {
+  ctx.body = cases;
+});
+
+
+const validateCreateCase = (body) => {
+    if (!body.date) {
+      return false;
+    }
+    return true;
+};
+
+
+router.post('/v1/cases', async (ctx) => {
+  // OK por ahora array global donde insertamos casos
+
+  if (validateCreateCase(ctx.request.body)) {
+    cases.push(ctx.request.body);
+    ctx.response.status=204;
+  } else {
+    ctx.response.status=422;
+    ctx.response.message="Missing date";
+  };
 });
 
 app
