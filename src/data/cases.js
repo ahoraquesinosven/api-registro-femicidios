89ff049ec459121a7c349b56c383b68dc02ee906
giselamo <gisemo@gmail..com>
2020-12-23 23:35:33 -0300

    Starts functions to extract data from API json

---
+++ src/data/cases.js
@@ -0,0 +1,12 @@
+funcion que reciba el json y mapee con la BD
+
+const saveCase = (case) => {
+  // BEGIN TRANSACTION
+    //saveVictim (case)  /
+    //saveAggressors /
+    //saveCase
+    // saveChildren
+    //saveCaseAggressors
+    //saveCaseNews
+
+}
