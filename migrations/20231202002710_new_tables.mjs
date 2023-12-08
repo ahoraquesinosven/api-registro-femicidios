56a94e1c02291d1756e3c6973965d86d45b1f889
Agus-Coder <agus_code@outlook.com.ar>
2023-12-08 00:00:44 -0300

    New tables, revisited
    Related to https://observatorioaqsnv.atlassian.net/browse/OBS-14 \n Se reemplazan columnas tipo string por specificType or text segun cada caso.\nEjemplo: 'case_place' es de type text

--- migrations/20231202002710_new_tables.mjs
+++ migrations/20231202002710_new_tables.mjs
@@ -9,15 +9,15 @@ export const up = async (knex) => {
   table.increments('victim_id', { primaryKey: true }).notNullable();
   table.date('victim_creation');
   table.date('victim_last_update');
-  table.string('victim_name_lastname', 50);
+  table.text('victim_name_lastname');
   table.integer('victim_age');
-  table.string('victim_nationality', 3);
+  table.string('victim_nationality', 3); 
   table.boolean('victim_prostitution');
   table.boolean('victim_missing');
   table.boolean('victim_native_people');
   table.boolean('victim_pregnant');
   table.boolean('victim_disabillity');
-  table.string('victim_ocupation', 50);
+  table.text('victim_ocupation');
   table.boolean('victim_children');
  });
 
@@ -25,8 +25,8 @@ export const up = async (knex) => {
   table.increments('aggresor_id', { primaryKey: true }).notNullable();
   table.date('aggresor_creation');
   table.date('aggresor_last_update');
-  table.string('aggresor_gender', 3);
-  table.string('aggresor_name_lastname', 50);
+  table.text('aggresor_gender');
+  table.text('aggresor_name_lastname');
   table.integer('aggresor_age');
   table.boolean('aggresor_legal_complaint_history');
   table.boolean('aggresor_cases_history');
@@ -43,25 +43,25 @@ knex.schema.withSchema('public').createTable('Cases', (table)=>{
   table.date('case_creation');
   table.date('last_update');
   table.date('incident_date');
-  table.string('case_day_moment', 30);
-  table.string('case_type', 20);
-  table.string('case_gender', 3);
-  table.string('case_province', 20);
-  table.string('case_location', 30);
-  table.string('case_geographic_ubication', 30);
-  table.string('case_place', 20);
-  table.string('case_form', 20);
+  table.specificType('case_day_moment', 'character varying');
+  table.specificType('case_type', 'character varying');
+  table.specificType('case_gender', 'character varying');
+  table.specificType('case_province', 'character varying');
+  table.specificType('case_location', 'character varying');
+  table.specificType('case_geographic_ubication', 'character varying');
+  table.text('case_place');
+  table.specificType('case_form', 'character varying');
   table.boolean('case_justice');
   table.integer('case_legal_complaints');
   table.boolean('case_rape');
   table.boolean('case_organized_crime');
-  table.string('case_organized_crime_notes', 255);
-  table.string('case_notes', 255);
-  table.string('case_news_links', 255);
+  table.text('case_organized_crime_notes');
+  table.text('case_notes');
+  table.text('case_news_links');
  })
 
  knex.schema.withSchema('public').createTable('Bonds', (table) => {
-   table.incremets('bond_id');
+   table.increments('bond_id');
    table.foreign('victim_id').references('Victims.victim_id'); 
    table.foreign('aggresor_id').references('Aggresors.aggresor_id');
  })
