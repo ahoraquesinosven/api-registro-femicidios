96976f641a99243a5fb82c4e27299df57e64f198
Andres Arana <and2arana@gmail.com>
2024-06-22 02:46:49 -0300

    Adds simple error handling logger

--- src/middleware/log.js
+++ src/middleware/log.js
@@ -11,23 +11,32 @@ const decideLogLevel = (status) => {
 export function logRequest() {
   return async function logRequest(ctx, next) {
     const start = new Date();
-    await next();
-    const durationMs = new Date() - start;
+    try {
+      await next();
+    } catch (err) {
+      logger.error({
+        message: err.message,
+        stack: err.stack,
+      });
+      ctx.status = 500;
+    } finally {
+      const durationMs = new Date() - start;
 
-    const httpRequest = {
-      requestMethod: ctx.request.method,
-      requestUrl: ctx.request.url,
-      requestSize: ctx.request.length,
-      status: ctx.response.status,
-      responseSize: ctx.response.length,
-      userAgent: ctx.request.get("user-agent"),
-      remoteIp: ctx.request.ip,
-      latency: `${durationMs / 1000}s`,
-    };
+      const httpRequest = {
+        requestMethod: ctx.request.method,
+        requestUrl: ctx.request.url,
+        requestSize: ctx.request.length,
+        status: ctx.response.status,
+        responseSize: ctx.response.length,
+        userAgent: ctx.request.get("user-agent"),
+        remoteIp: ctx.request.ip,
+        latency: `${durationMs / 1000}s`,
+      };
 
-    logger.log(
-      decideLogLevel(ctx.response.status),
-      {httpRequest},
-    );
+      logger.log(
+        decideLogLevel(ctx.response.status),
+        {httpRequest},
+      );
+    }
   };
 }
