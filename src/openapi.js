5180fdacbed827f5d0160035c89df8b3251f60e7
Andres Arana <and2arana@gmail.com>
2024-11-06 01:07:24 -0300

    Refactors router to support ajv validation

--- src/openapi.js
+++ src/openapi.js
@@ -1,48 +1,53 @@
 import Router from "@koa/router";
 import config from "./config/values.js";
+import {requireUserAuth, requireServerAuth} from "./middleware/auth.js";
 
-class OpenApiDocument {
-  document = {
-    openapi: "3.1.0",
-    info: {
-      title: "AQSNV - Registro de Femicidios",
-      version: "1.0.0",
-    },
-    servers: [
-      { url: config.server.cannonicalOrigin },
-    ],
-    components: {
-      securitySchemes: {
-        oauth: {
-          type: "oauth2",
-          description: "OAuth2.0 security scheme used for public endpoints",
-          flows: {
-            authorizationCode: {
-              authorizationUrl: "/auth/authorize",
-              tokenUrl: "/auth/token",
-              scopes: {},
-            },
-          },
-        },
-
-        internal: {
-          type: "apiKey",
-          description: "API Key security scheme used for internal endpoints",
-          name: "authorization",
-          in: "header",
+export const securitySchemes = {
+  oauth: {
+    component: {
+      type: "oauth2",
+      description: "OAuth2.0 security scheme used for public endpoints",
+      flows: {
+        authorizationCode: {
+          authorizationUrl: "/auth/authorize",
+          tokenUrl: "/auth/token",
+          scopes: {},
         },
       },
     },
-  }
+    endpointSpec: [{oauth: []}],
+    middleware: requireUserAuth,
+  },
 
-  registerOperation(path, method, doc) {
-    const pathsObject = (this.document.paths ||= {});
-    const pathObject = (pathsObject[path] ||= {});
-    pathObject[method] = doc;
-  }
-}
+  internal: {
+    component: {
+      type: "apiKey",
+      description: "API Key security scheme used for internal endpoints",
+      name: "authorization",
+      in: "header",
+    },
+    endpointSpec: [{internal: []}],
+    middleware: requireServerAuth,
+  },
+};
 
-export const openApiDocument = new OpenApiDocument();
+export const openApiDocument = {
+  openapi: "3.1.0",
+  info: {
+    title: "AQSNV - Registro de Femicidios",
+    version: "1.0.0",
+  },
+  servers: [
+    {url: config.server.cannonicalOrigin},
+  ],
+  components: {
+    securitySchemes: {
+      oauth: securitySchemes.oauth.component,
+      internal: securitySchemes.internal.component,
+    },
+  },
+  paths: {},
+};
 
 export class OpenApiRouter {
   nativeRouter;
@@ -56,19 +61,29 @@ export class OpenApiRouter {
     });
   }
 
-  operation(options) {
-    const pathForOpenApi = `${this.prefix}${options.relativePath}`;
-    const pathForRouter = options.relativePath.replaceAll(/{(\w+)}/g, (_, group) => `:${group}`);
+  registerNativeRoute(options) {
+    const path = options.relativePath.replaceAll(/{(\w+)}/g, (_, group) => `:${group}`);
+    const securityHandlers = options.spec.security ? [options.spec.security.middleware] : [];
     this.nativeRouter[options.method](
-      pathForRouter,
+      path,
+      ...securityHandlers,
       ...options.handlers,
     );
+  }
 
-    openApiDocument.registerOperation(
-      pathForOpenApi,
-      options.method,
-      options.spec,
-    );
+  registerOpenApiDocumentPath(options) {
+    const path = `${this.prefix}${options.relativePath}`;
+    const pathObject = (openApiDocument.paths[path] ||= {});
+    const security = options.spec.security ? { security: options.spec.security.endpointSpec } : {};
+    pathObject[options.method] = {
+      ...options.spec,
+      ...security,
+    };
+  }
+
+  operation(options) {
+    this.registerNativeRoute(options);
+    this.registerOpenApiDocumentPath(options);
   }
 }
 
--- src/routers/cases.js
+++ src/routers/cases.js
@@ -1,5 +1,4 @@
-import { OpenApiRouter } from "../openapi.js";
-import { requireUserAuth } from "../middleware/auth.js";
+import {OpenApiRouter, securitySchemes} from "../openapi.js";
 import knex from "../services/knex.js";
 
 const router = new OpenApiRouter({
@@ -13,60 +12,58 @@ const pick = (obj, keys) => Object.fromEntries(
 );
 
 router.operation({
-  relativePath: "/",
-  method: "post",
-  spec: {
+  method: "post", relativePath: "/", spec: {
     tags: ["cases"],
     summary: "Create a new case",
-    security: [{"oauth": []}],
+    security: securitySchemes.oauth,
     requestBody: {
       content: {
         "application/json": {
           schema: {
             type: "object",
             properties: {
-              occurredAt: { type: "string", format: "date" },
-              momentOfDay: { type: "string" },
-              province: { type: "string" },
-              location: { type: "string" },
-              geographicLocation: { type: "string" },
-              place: { type: "string" },
-              murderWeapon: { type: "string" },
-              wasJudicialized: { type: "boolean" },
-              hadLegalComplaints: { type: "boolean" },
-              isRape: { type: "boolean" },
-              isRelatedToOrganizedCrime: { type: "boolean" },
-              organizedCrimeNotes: { type: "string" },
-              generalNotes: { type: "string" },
-              newsLinks: { type: "array", items: { type: "string" } },
+              occurredAt: {type: "string", format: "date"},
+              momentOfDay: {type: "string"},
+              province: {type: "string"},
+              location: {type: "string"},
+              geographicLocation: {type: "string"},
+              place: {type: "string"},
+              murderWeapon: {type: "string"},
+              wasJudicialized: {type: "boolean"},
+              hadLegalComplaints: {type: "boolean"},
+              isRape: {type: "boolean"},
+              isRelatedToOrganizedCrime: {type: "boolean"},
+              organizedCrimeNotes: {type: "string"},
+              generalNotes: {type: "string"},
+              newsLinks: {type: "array", items: {type: "string"}},
 
               victim: {
                 type: "object",
                 properties: {
-                  fullName: { type: "string" },
-                  age: { type: "integer" },
-                  gender: { type: "string" },
-                  nationality: { type: "string", minLength: 3, maxLength: 3 },
-                  isSexualWorker: { type: "boolean" },
-                  isMissingPerson: { type: "boolean" },
-                  isNativePeople: { type: "boolean" },
-                  isPregnant: { type: "boolean" },
-                  hasDisabillity: { type: "boolean" },
-                  occupation: { type: "string" },
-                  hasChildren: { type: "boolean" },
+                  fullName: {type: "string"},
+                  age: {type: "integer"},
+                  gender: {type: "string"},
+                  nationality: {type: "string", minLength: 3, maxLength: 3},
+                  isSexualWorker: {type: "boolean"},
+                  isMissingPerson: {type: "boolean"},
+                  isNativePeople: {type: "boolean"},
+                  isPregnant: {type: "boolean"},
+                  hasDisabillity: {type: "boolean"},
+                  occupation: {type: "string"},
+                  hasChildren: {type: "boolean"},
                 },
               },
 
               aggressor: {
                 type: "object",
                 properties: {
-                  fullName: { type: "string" },
-                  age: { type: "integer" },
-                  gender: { type: "string" },
-                  hasLegalComplaintHistory: { type: "boolean" },
-                  hasPreviousCases: { type: "boolean" },
-                  wasInPrison: { type: "boolean" },
-                  behaviourPostCase: { type: "string" },
+                  fullName: {type: "string"},
+                  age: {type: "integer"},
+                  gender: {type: "string"},
+                  hasLegalComplaintHistory: {type: "boolean"},
+                  hasPreviousCases: {type: "boolean"},
+                  wasInPrison: {type: "boolean"},
+                  behaviourPostCase: {type: "string"},
                 },
               },
             },
@@ -83,64 +80,62 @@ router.operation({
       },
     },
   },
-  handlers: [
-    requireUserAuth,
-    async (ctx) => {
-      const body = ctx.request.body;
+  handlers: [async (ctx) => {
+    const body = ctx.request.body;
 
-      await knex.transaction(async (trx) => {
-        const [{id: victimId}] = await trx("victims")
-          .insert(pick(body.victim, [
-            "fullName",
-            "age",
-            "gender",
-            "nationality",
-            "isSexualWorker",
-            "isMissingPerson",
-            "isNativePeople",
-            "isPregnant",
-            "hasDisabillity",
-            "occupation",
-            "hasChildren",
-          ]))
-          .returning("id");
+    await knex.transaction(async (trx) => {
+      const [{id: victimId}] = await trx("victims")
+        .insert(pick(body.victim, [
+          "fullName",
+          "age",
+          "gender",
+          "nationality",
+          "isSexualWorker",
+          "isMissingPerson",
+          "isNativePeople",
+          "isPregnant",
+          "hasDisabillity",
+          "occupation",
+          "hasChildren",
+        ]))
+        .returning("id");
 
-        const [{id: aggressorId}] = await trx("aggressors")
-          .insert(pick(body.aggressor, [
-            "fullName",
-            "age",
-            "gender",
-            "hasLegalComplaintHistory",
-            "hasPreviousCases",
-            "wasInPrison",
-            "behaviourPostCase",
-          ]))
-          .returning("id");
+      const [{id: aggressorId}] = await trx("aggressors")
+        .insert(pick(body.aggressor, [
+          "fullName",
+          "age",
+          "gender",
+          "hasLegalComplaintHistory",
+          "hasPreviousCases",
+          "wasInPrison",
+          "behaviourPostCase",
+        ]))
+        .returning("id");
 
-        await trx("cases").insert({
-          ...pick(body, [
-            "occurredAt",
-            "momentOfDay",
-            "province",
-            "location",
-            "geographicLocation",
-            "place",
-            "murderWeapon",
-            "wasJudicialized",
-            "hadLegalComplaints",
-            "isRape",
-            "isRelatedToOrganizedCrime",
-            "organizedCrimeNotes",
-            "generalNotes",
-            "newsLinks",
-          ]),
-          aggressorId,
-          victimId,
-        });
+      await trx("cases").insert({
+        ...pick(body, [
+          "occurredAt",
+          "momentOfDay",
+          "province",
+          "location",
+          "geographicLocation",
+          "place",
+          "murderWeapon",
+          "wasJudicialized",
+          "hadLegalComplaints",
+          "isRape",
+          "isRelatedToOrganizedCrime",
+          "organizedCrimeNotes",
+          "generalNotes",
+          "newsLinks",
+        ]),
+        aggressorId,
+        victimId,
       });
+    });
 
-      ctx.status = 201;
-    },
+    ctx.status = 201;
+  },
   ],
 });
 
