7f0737596dfcc1fcad921058aeb17b1661d2d3c3
Gisela Morinigo <gisemo@gmail.com>
2026-04-19 22:47:12 -0300

    Replaces case schema separated from the POST

--- src/routers/cases.js
+++ src/routers/cases.js
@@ -22,81 +22,7 @@ router.operation({
       required: true,
       content: {
         "application/json": {
-          schema: {
-            type: "object",
-            required: ["occurredAt", "province", "place", "newsLinks", "victim", "aggressor", "caseCategory"],
-            dependentRequired: {
-              organizedCrimeNotes: ["isRelatedToOrganizedCrime"],
-              totalLegalComplaints: ["hadLegalComplaints"],
-              wasJudicialized: ["hadLegalComplaints"],
-              judicialMeasures: ["wasJudicialized"],
-            },
-
-            properties: {
-              caseCategory: { $ref: "#/components/schemas/CaseCategory" },
-              wasItAnAttempt: { type: "boolean" },
-              isInsufficientDataOrUnderInvestigation: { type: "boolean" },
-              occurredAt: { type: "string", format: "date" },
-              momentOfDay: { $ref: "#/components/schemas/CaseMomentOfDay" },
-              province: { $ref: "#/components/schemas/Province" },
-              location: { type: "string"},
-              geographicLocation: { $ref: "#/components/schemas/CaseGeographicLocation" },
-              place: { $ref: "#/components/schemas/CasePlace" },
-              murderWeapon: { $ref: "#/components/schemas/CaseMurderWeapon" },
-              hadLegalComplaints: { type: "boolean" },
-              totalLegalComplaints: { type: "integer" },
-              wasJudicialized: { type: "boolean" }, //¿Había alguna medida judicial?
-              judicialMeasures: { type: "array", items: { $ref: "#/components/schemas/CaseJudicialMeasure" } },
-              victimBondAggressor: { $ref: "#/components/schemas/CaseVictimBondAggressor" },
-              isRape: { type: "boolean" },
-              isRelatedToOrganizedCrime: { type: "boolean" },
-              organizedCrimeNotes: { type: "string" },
-              generalNotes: { type: "string" },
-              newsLinks: { type: "array", items: { type: "string" }, minItems: 1, "uniqueItems": true },
-
-
-
-              victim: {
-                type: "object",
-                properties: {
-                  fullName: { type: "string", minLength: 5 },
-                  age: { type: "integer" },
-                  gender: { $ref: "#/components/schemas/Gender" },
-                  nationality: { $ref: "#/components/schemas/Nationality" },
-                  isSexualWorker: { type: "boolean" },
-                  isMissingPerson: { type: "boolean" },
-                  isNativePeople: { type: "boolean" },
-                  isPregnant: { type: "boolean" },
-                  hasDisabillity: { type: "boolean" },
-                  occupation: { type: "string" },
-                  hasChildren: { type: "boolean" },
-                  numberOfChildren: { type: "integer" },
-                  ageOfChildren: { type: "array", items: { type: "number" }, minItems: 0, "uniqueItems": false },
-                },
-                additionalProperties: false,
-              },
-
-              aggressor: {
-                type: "object",
-                dependentRequired: {
-                  securityForce: ["belongsSecurityForce"],
-                },
-                properties: {
-                  fullName: { type: "string", minLength: 5 },
-                  age: { type: "integer" },
-                  gender: { $ref: "#/components/schemas/Gender" },
-                  hasLegalComplaintHistory: { type: "boolean" },
-                  hasPreviousCases: { type: "boolean" },
-                  wasInPrison: { type: "boolean" },
-                  behaviourPostCase: { $ref: "#/components/schemas/CaseAggressorBehaviorPostCase" },
-                  belongsSecurityForce: { type: "boolean" },
-                  securityForce: { $ref: "#/components/schemas/CaseAggressorSecurityForce" },
-                },
-                additionalProperties: false,
-              },
-            },
-            additionalProperties: false,
-          },
+          schema: { $ref: "#/components/schemas/Case" },
         },
       },
     },
@@ -114,7 +40,7 @@ router.operation({
       // More complex validations
       const errors = [];
 
-//Case validations
+      //Case validations
       if (body.organizedCrimeNotes && !body.isRelatedToOrganizedCrime) {
         errors.push({
           "type": "body",
@@ -163,7 +89,7 @@ router.operation({
         });
       }
 
-//victim validations
+      //victim validations
       if (body.victim.numberOfChildren && !body.victim.hasChildren) {
         errors.push({
           "type": "body",
@@ -180,7 +106,7 @@ router.operation({
         });
       }
 
-//aggresor validations
+      //aggresor validations
       if (body.aggressor.securityForce && !body.aggressor.belongsSecurityForce) {
         errors.push({
           "type": "body",
