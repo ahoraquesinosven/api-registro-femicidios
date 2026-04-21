4db57e56d77635d8c4d2996fa424fc27201ae0d4
Gisela Morinigo <gisemo@gmail.com>
2026-04-21 00:14:22 -0300

    Adds comment to the knex debug

--- src/services/knex.js
+++ src/services/knex.js
@@ -4,5 +4,5 @@ import config from '../config/values.js';
 export default new Knex({
   client: 'pg',
   connection: config.db.connectionString,
-  debug:true
+  //debug:true enable this during develoment when you want to see all queries
 });
