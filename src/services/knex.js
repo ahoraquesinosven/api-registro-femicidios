03ac2d07640a0a17a319e49ed303e5a8668a5ba1
Andres Arana <and2arana@gmail.com>
2026-05-15 11:39:00 -0300

    Adds name tokenization to fullname queries

--- src/services/knex.js
+++ src/services/knex.js
@@ -1,50 +1,74 @@
 import Knex from "knex";
 import config from '../config/values.js';
 
-export default new Knex({
+const knex = new Knex({
   client: 'pg',
   connection: config.db.connectionString,
-  //debug:true enable this during develoment when you want to see all queries
+  // enable this during develoment when you want to see all queries
+  // debug: true 
 });
 
-export class NestedObjectsQuery {
-  static ALIAS_SEPARATOR = "_"
+export default knex;
 
-  constructor({baseQuery, rootQualifier, fields}) {
-    this.baseQuery = baseQuery;
+const unaccentedMatchExpression = (field, value) => knex.raw(
+  "unaccent(:field:) ILIKE unaccent(:value)",
+  {
+    field,
+    value: `%${value}%`,
+  }
+);
 
-    this.fields = fields.map((field) => ({
-      rawExpression: field,
-      alias: field.replaceAll(".", NestedObjectsQuery.ALIAS_SEPARATOR),
-    }));
+Knex.QueryBuilder.extend("whereUnaccentedMatch", function (field, value) {
+  return this.where(unaccentedMatchExpression(field, value));
+});
 
-    this.rootQualifier = rootQualifier;
+Knex.QueryBuilder.extend("orWhereUnaccentedMatch", function (field, value) {
+  return this.orWhere(unaccentedMatchExpression(field, value));
+});
+
+Knex.QueryBuilder.extend("whereNameMatch", function (field, value) {
+  const tokens = value
+    .split(" ")
+    .map((token) => token.trim())
+    .filter((token) => token && token !== "");
+
+  if (tokens.length > 0) {
+    return this.where((builder) => {
+      tokens.reduce(
+        (expression, token) => expression.orWhereUnaccentedMatch(field, token),
+        builder,
+      );
+    });
   }
 
-  selectExpression() {
-    return Object.fromEntries(
-      this.fields.map((field) => [field.alias, field.rawExpression])
-    );
-  }
+  return this;
+});
 
-  toNestedObjects(row) {
-    const result = {};
-    for (const field of this.fields) {
-      const [qualifier, attribute] = field.alias.split(NestedObjectsQuery.ALIAS_SEPARATOR);
+const ALIAS_SEPARATOR = "_";
+
+Knex.QueryBuilder.extend("toNestedObjects", async function ({rootQualifier, fields}) {
+  const fieldInfo = fields.map((field) => ({
+    rawExpression: field,
+    alias: field.replaceAll(".", ALIAS_SEPARATOR),
+  }));
+
+  const selectExpression = Object.fromEntries(
+    fieldInfo.map((field) => [field.alias, field.rawExpression])
+  );
+
+  const rows = await this.select(selectExpression);
+
+  return rows.map((row) => fieldInfo.reduce((result, field) => {
+      const [qualifier, attribute] = field.alias.split(ALIAS_SEPARATOR);
       const value = row[field.alias];
 
-      if (qualifier === this.rootQualifier) {
+      if (qualifier === rootQualifier) {
         result[attribute] = value;
       } else {
         result[qualifier] ||= {};
         result[qualifier][attribute] = value;
       }
-    }
-    return result;
-  }
 
-  async select() {
-    const rows = await this.baseQuery.select(this.selectExpression());
-    return rows.map((row) => this.toNestedObjects(row));
-  }
-}
+    return result;
+  }, {}));
+});
