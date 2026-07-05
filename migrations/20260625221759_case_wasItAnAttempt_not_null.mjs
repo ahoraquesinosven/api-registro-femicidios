2e1b98e0529e3b140248a269d856fcdebfad931f
Gisela Morinigo <gisemo@gmail.com>
2026-07-05 11:34:56 -0300

    Fixes format

--- migrations/20260625221759_case_wasItAnAttempt_not_null.mjs
+++ migrations/20260625221759_case_wasItAnAttempt_not_null.mjs
@@ -1,9 +1,7 @@
 export const up = async (knex) => {
-
   await knex("cases")
     .whereNull("wasItAnAttempt")
     .update({ wasItAnAttempt: false });
-
 
   await knex.schema.withSchema("public").alterTable("cases", (table) => {
     table.boolean("wasItAnAttempt").notNullable().alter();
